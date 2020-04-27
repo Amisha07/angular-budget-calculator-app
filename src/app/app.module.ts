6c021dbd8591cb4a025eeafec85c1ed8ee041b28
Amisha07 <amishagadhia1707@gmail.com>
2020-04-27 16:25:14 -0500

    3 files modified, 11 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { BudgetCalculatorComponent } from './budget-calculator/budget-calculator.component';
+import { AddItemComponent } from './add-item/add-item.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { ItemCardComponent } from './item-list/item-card/item-card.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BudgetCalculatorComponent ],
+  declarations: [ AppComponent, HelloComponent, BudgetCalculatorComponent, AddItemComponent, ListItemComponent, ItemCardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/budget-calculator/budget-calculator.component.ts
+++ src/app/budget-calculator/budget-calculator.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'budget-calculator',
   templateUrl: './budget-calculator.component.html',
-  styleUrls: ['./budget-calculator.component.css']
+  styleUrls: ['./budget-calculator.component.scss']
 })
 export class BudgetCalculatorComponent implements OnInit {
 
